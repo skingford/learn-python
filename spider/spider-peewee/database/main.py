67840d27026357e305af60fa35dac2244419fbe9
SteveKingFord <332312568@qq.com>
2022-01-29 00:58:12 +0800

    feat: crud

--- spider/spider-peewee/database/main.py
+++ spider/spider-peewee/database/main.py
@@ -12,5 +12,16 @@ class Person(Model):
         table_name = "users"
 
 
+def crud():
+    from datetime import date
+    uncle_bob = Person(name='kf', birthday=date(1994, 10, 9))
+    uncle_bob.save()
+
+    kf = Person.select().where(Person.name == 'kf').get()
+
+    print(kf)
+
+
 if __name__ == "__main__":
-    db.create_tables([Person])
+    # db.create_tables([Person])
+    crud()
