3cee642ed2bdb96446704d3c13e798d5c46afcb8
SteveKingFord <332312568@qq.com>
2022-01-30 13:34:31 +0800

    feat: csdn full url

--- spider/spider-peewee/csdn.py
+++ spider/spider-peewee/csdn.py
@@ -2,6 +2,9 @@ import ast
 import re
 
 import requests
+from urllib import parse
+
+domain = "https://bbs.csdn.net"
 
 
 def get_nodes_json():
@@ -19,13 +22,39 @@ url_list = []
 
 def process_nodes_list(nodes):
     for item in nodes:
-        if "url" in item:
+        if "url" in item and item["url"]:
             url_list.append(item["url"])
             if "children" in item:
                 process_nodes_list(item["children"])
 
 
-if __name__ == "__main__":
+def get_level1_list(nodes):
+    level1_url = []
+    for item in nodes:
+        if "url" in item and item["url"]:
+            level1_url.append(item["url"])
+    return level1_url
+
+
+def get_last_urls():
     nodes_list = get_nodes_json()
     process_nodes_list(nodes_list)
-    print(url_list)
+    level1_list = get_level1_list(nodes_list)
+    last_urls = []
+    for url in url_list:
+        if url not in level1_list:
+            full_url = parse.urljoin(domain, url)
+            last_urls.append(full_url)
+
+    all_urls = []
+    for url in last_urls:
+        all_urls.append(url)
+        all_urls.append(url + "/recommend")
+        all_urls.append(url + "/closed")
+    return all_urls
+
+
+if __name__ == "__main__":
+    lst = get_last_urls()
+    print(lst)
+    print(len(lst))
